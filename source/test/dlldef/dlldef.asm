f2bbaffb923709cf541b32d316e9cef25a086429
nidud <nidud@hotmail.com>
2017-06-06 05:34:04 +0200

    dlldef

--- source/test/dlldef/dlldef.asm
+++ source/test/dlldef/dlldef.asm
@@ -7,35 +7,31 @@ include tchar.inc
     .code
 
 main proc
-    mov rsi,rdx
     .if rcx == 2
-        mov rdi,[rsi+8]
-        .if LoadLibrary(rdi)
-            mov r12,rax
-            mov rbx,strcpy(strrchr(rdi,'.'),".def")
-            .if fopen(rdi,"wt")
-                mov r13,rax
-                mov byte ptr [rbx],0
-                fprintf(r13,"LIBRARY %s\nEXPORTS\n",rdi)
-                mov rdi,r12
-                add edi,[rdi+0x3C]
-                mov edi,[rdi].IMAGE_NT_HEADERS.OptionalHeader.DataDirectory.VirtualAddress
-                add rdi,r12
-                mov ebx,[rdi+0x18]
-                mov esi,[rdi+0x20]
-                add rsi,r12
-                .while ebx
-                    lodsd
-                    add rax,r12
-                    fprintf(r13,"\"%s\"\n",rax)
-                    dec ebx
-                .endw
-                fclose(r13)
-            .endif
-            FreeLibrary(r12)
-        .endif
+	mov rdi,[rdx+8]
+	.if LoadLibrary(rdi)
+	    mov r12,rax
+	    mov rbx,strcpy(strrchr(rdi,'.'),".def")
+	    .if fopen(rdi,"wt")
+		mov r13,rax
+		mov byte ptr [rbx],0
+		fprintf(r13,"LIBRARY %s\nEXPORTS\n",rdi)
+		mov eax,[r12+0x3C]
+		mov eax,[r12+rax].IMAGE_NT_HEADERS.OptionalHeader.DataDirectory.VirtualAddress
+		mov ebx,[r12+rax+0x18]
+		mov esi,[r12+rax+0x20]
+		add rsi,r12
+		.while ebx
+		    lodsd
+		    fprintf(r13,"\"%s\"\n",addr [rax+r12])
+		    dec ebx
+		.endw
+		fclose(r13)
+	    .endif
+	    FreeLibrary(r12)
+	.endif
     .else
-        printf("\nUsage: DLLDEF <dllname>.dll\n\n")
+	printf("\nUsage: DLLDEF <dllname>.dll\n\n")
     .endif
     xor eax,eax
     ret

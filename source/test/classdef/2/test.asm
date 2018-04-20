73301ac5871f963a81d665772f8f6d1988f5f4eb
nidud <nidud@hotmail.com>
2018-04-20 15:08:15 +0200

    2.27.11

--- source/test/classdef/2/test.asm
+++ source/test/classdef/2/test.asm
@@ -1,89 +1,35 @@
-include stdio.inc
+include tchar.inc
+include iconfig.inc
 
     .code
 
-.classdef XPush :UINT
-
-    base    QWORD ?
-    buffer  OWORD 8 dup(?)
-
-    Release proc
-    .ends
-
-    assume rcx:LPXPUSH
-
-    option win64:rsp nosave noauto
-
-XPush::Release proc
-
-    movaps xmm0,[rcx].buffer[0x00]
-    movaps xmm1,[rcx].buffer[0x10]
-    movaps xmm2,[rcx].buffer[0x20]
-    movaps xmm3,[rcx].buffer[0x30]
-    movaps xmm4,[rcx].buffer[0x40]
-    movaps xmm5,[rcx].buffer[0x50]
-    movaps xmm6,[rcx].buffer[0x60]
-    movaps xmm7,[rcx].buffer[0x70]
-    mov rax,[rsp]
-    mov rsp,[rcx].base
-    jmp rax
-
-XPush::Release endp
-
-XPush::XPush proc ReservedStack:UINT
-
-    .repeat
-
-        .if !rcx
-
-            mov r8,rsp
-            lea rax,[r8+rdx]        ; stack base
-            lea rcx,[rdx+8*16+16]   ; alloc size
-            sub rax,rcx
-            and al,-16
-            test [rax],al           ; probe..
-            mov rcx,[r8]            ; return addr
-            mov rsp,rax             ; new stack
-            mov [rax],rcx
-            and dl,-16              ; shadow space
-            lea rcx,[rax+rdx+16]
-            add r8,8
-            mov [rcx].base,r8
-        .endif
-
-        movaps [rcx].buffer[0x00],xmm0
-        movaps [rcx].buffer[0x10],xmm1
-        movaps [rcx].buffer[0x20],xmm2
-        movaps [rcx].buffer[0x30],xmm3
-        movaps [rcx].buffer[0x40],xmm4
-        movaps [rcx].buffer[0x50],xmm5
-        movaps [rcx].buffer[0x60],xmm6
-        movaps [rcx].buffer[0x70],xmm7
-
-        lea rdx,[rax+0x80]
-        mov [rcx],rdx
-        lea rax,XPush@Release
-        mov [rdx],rax
-        mov rax,rcx
-    .until 1
-    ret
-
-XPush::XPush endp
-
-    option win64:rbp auto
+    assume rcx:ptr IConfig
+    assume rbx:ptr IConfig
 
 main proc
 
-  local p:LPXPUSH
+    mov rbx, IConfig::IConfig( NULL )
 
-    printf("RSP: %p\n", rsp)
-    mov p,XPush::XPush( NULL, @ReservedStack )
-    printf("RSP: %p\n", rsp)
-    p.Release()
-    printf("RSP: %p\n", rsp)
+    [rbx].Create( "Version" )
+    [rcx].Create( "Base=%d.%d", 1, 0 )
+    [rbx].Create( "Product" )
+    [rcx].Create( "Source=AsmcOOP" )
+    [rbx].Create( "URL" )
+    [rcx].Create( "UpdateURL=http://masm32.com/board/index.php?topic=7000.0" )
+    [rbx].Find  ( "Version" )
+    [rcx].Create( "Package=%d.%d", 1, 1 )
+    [rbx].Write ( "test.ini" )
+    [rbx].Release()
+
+    mov rbx, IConfig::IConfig( NULL )
+
+    [rbx].Read  ( "test.ini" )
+    [rbx].Write ( "test.ini" )
+    [rbx].Release()
+
+    xor eax,eax
     ret
 
 main endp
 
-    end main
-
+    end _tstart

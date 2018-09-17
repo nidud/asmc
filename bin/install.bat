3014318f16f95e89baef7f6cad360d76fd22e33f
nidud <nidud@hotmail.com>
2018-09-17 20:47:07 +0200

    2.28.08
    install.bat

--- bin/install.bat
+++ bin/install.bat
@@ -9,3 +9,7 @@ cd ..\..\source\lib32
 ..\..\bin\dz make.exe
 cd ..\lib64
 ..\..\bin\dz make.exe
+cd ..\directx
+..\..\bin\dz make.exe
+cd ..\uuid
+..\..\bin\dz make.exe
--- source/tools/linkw/linkw/src/h/banner.h
+++ source/tools/linkw/linkw/src/h/banner.h
@@ -72,7 +72,7 @@
 #define _RC_DELIM_LEFT_		[
 #define _RC_DELIM_RIGHT_	]
 
-#define BAN_VER_STR "2.0"
+#define BAN_VER_STR "2.01"
 #define _WLINK_VERSION_		BAN_VER_STR
 
 

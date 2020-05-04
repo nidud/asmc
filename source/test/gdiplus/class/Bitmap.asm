a58cc900c08a9534e57f88d23f86b675ea39b0d3
nidud <nidud@hotmail.com>
2020-05-04 18:46:06 +0200

    2.31.26

---
+++ source/test/gdiplus/class/Bitmap.asm
@@ -0,0 +1,79 @@
+; BITMAP.ASM--
+;
+; Copyright (c) The Asmc Contributors. All rights reserved.
+; Consult your license regarding permissions and restrictions.
+;
+
+include windows.inc
+include gdiplus.inc
+
+    .code
+
+main proc
+
+  .new p:ptr Bitmap()
+
+    Bitmap()
+    p.Bitmap()
+    p.Release()
+
+    p.FromFile(NULL, FALSE)
+    p.FromStream(NULL, FALSE)
+    p.Clone()
+    p.Save(NULL, NULL, NULL)
+    p.Save2(NULL, NULL, NULL)
+    p.SaveAdd(NULL)
+    p.SaveAdd2(NULL, NULL)
+    p.GetType()
+    p.GetPhysicalDimension(NULL)
+    p.GetBounds(NULL, NULL)
+    p.GetWidth()
+    p.GetHeight()
+    p.GetHorizontalResolution()
+    p.GetVerticalResolution()
+    p.GetFlags()
+    p.GetRawFormat(NULL)
+    p.GetPixelFormat()
+    p.GetPaletteSize()
+    p.GetPalette(NULL, 0)
+    p.SetPalette(NULL)
+    p.GetThumbnailImage(0, 0, 0, NULL)
+    p.GetFrameDimensionsCount()
+    p.GetFrameDimensionsList(NULL, 0)
+    p.GetFrameCount(NULL)
+    p.SelectActiveFrame(NULL, 0)
+    p.RotateFlip(NULL)
+    p.GetPropertyCount()
+    p.GetPropertyIdList(0, NULL)
+    p.GetPropertyItemSize(NULL)
+    p.GetPropertyItem(NULL, 0, NULL)
+    p.GetPropertySize(NULL, NULL)
+    p.GetAllPropertyItems(0, 0, NULL)
+    p.RemovePropertyItem(NULL)
+    p.SetPropertyItem(NULL)
+    p.GetEncoderParameterListSize(NULL)
+    p.GetEncoderParameterList(NULL, 0, NULL)
+if (GDIPVER GE 0x0110)
+    p.FindFirstItem(NULL)
+    p.FindNextItem(NULL)
+    p.GetItemData(NULL)
+    p.SetAbort(NULL)
+endif
+    p.SetNativeImage(NULL)
+    p.SetStatus(0)
+    p.GetLastStatus()
+
+    p.FromScan0(0, 0, 0, 0, NULL)
+    p.FromScan02(0, 0, 0)
+    p.FromGraphics(0, 0, NULL)
+    p.FromDirectDrawSurface7(NULL)
+    p.FromBITMAPINFO(NULL, NULL)
+    p.FromHBITMAP(NULL, NULL)
+    p.FromHICON(NULL)
+    p.FromResource(NULL, NULL)
+
+    ret
+
+main endp
+
+    end
---
+++ source/test/gdiplus/class/Pen.asm
@@ -0,0 +1,73 @@
+; PEN.ASM--
+;
+; Copyright (c) The Asmc Contributors. All rights reserved.
+; Consult your license regarding permissions and restrictions.
+;
+
+include windows.inc
+include gdiplus.inc
+
+    .code
+
+main proc
+
+  local a:Pen
+
+    a.Pen()
+
+    a.Pen1(NULL, 1.0)
+
+    a.mequ84(&a, 0)
+    a.Pen1(NULL, 1.0)
+    a.Pen2(NULL, 1.0)
+    a.Release()
+    a.Clone()
+    a.SetWidth(1.0)
+    a.GetWidth()
+    a.SetLineCap(0, 1, 2)
+    a.SetStartCap(0)
+    a.SetEndCap(0)
+    a.SetDashCap(0)
+    a.GetStartCap()
+    a.GetEndCap()
+    a.GetDashCap()
+    a.SetLineJoin(0)
+    a.GetLineJoin()
+    a.SetCustomStartCap(NULL)
+    a.GetCustomStartCap(NULL)
+    a.SetCustomEndCap(NULL)
+    a.GetCustomEndCap(NULL)
+    a.SetMiterLimit(1.0)
+    a.GetMiterLimit()
+    a.SetAlignment(NULL)
+    a.GetAlignment()
+    a.SetTransform(NULL)
+    a.GetTransform(NULL)
+    a.ResetTransform()
+    a.MultiplyTransform(NULL, 0)
+    a.TranslateTransform(1.0, 2.0, 0)
+    a.ScaleTransform(1.0, 2.0, 0)
+    a.RotateTransform(1.0, 0)
+    a.GetPenType()
+    a.SetColor(NULL)
+    a.SetBrush(NULL)
+    a.GetColor(NULL)
+    ;a.GetBrush()
+    a.GetDashStyle()
+    a.SetDashStyle(0)
+    a.GetDashOffset()
+    a.SetDashOffset(1.0)
+    a.SetDashPattern(NULL, 0)
+    a.GetDashPatternCount()
+    a.GetDashPattern(NULL, 0)
+    a.SetCompoundArray(1.0, 0)
+    a.GetCompoundArrayCount()
+    a.GetCompoundArray(1.0, 0)
+    a.GetLastStatus()
+    a.SetNativePen(&a)
+    a.SetStatus(0)
+    ret
+
+main endp
+
+    end

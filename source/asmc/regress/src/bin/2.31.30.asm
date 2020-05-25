4b481e518e08b466d60e263303ad1b00a8dfbd84
nidud <nidud@hotmail.com>
2020-05-25 19:27:35 +0200

    2.31.32

--- source/asmc/regress/src/bin/2.31.30.asm
+++ source/asmc/regress/src/bin/2.31.30.asm
@@ -32,6 +32,24 @@ main proc
     mov C.lpVtbl,rax    ; lpVtbl added to class
     mov C.atom,al
 
+    movq    xmm0,3FF0000000000000r
+    movsd   xmm0,1.0
+    addsd   xmm0,1.0
+    subsd   xmm0,1.0
+    mulsd   xmm0,1.0
+    divsd   xmm0,1.0
+    comisd  xmm0,1.0
+    ucomisd xmm0,4.0 / 2.0 - 1.0
+
+    movd    xmm0,3F800000r
+    movss   xmm0,1.0
+    addss   xmm0,1.0
+    subss   xmm0,1.0
+    mulss   xmm0,1.0
+    divss   xmm0,1.0
+    comiss  xmm0,1.0
+    ucomiss xmm0,4.0 / 2.0 - 1.0
+
     ret
 
 main endp

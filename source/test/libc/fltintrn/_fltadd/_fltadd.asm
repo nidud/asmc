0cc8a74ee00f85a69be316eb2a695c23bb505e82
nidud <nidud@hotmail.com>
2024-11-04 06:02:46 +0100

    2.36.04

---
+++ source/test/libc/fltintrn/_fltadd/_fltadd.asm
@@ -0,0 +1,24 @@
+; _FLTADD.ASM--
+;
+; Copyright (c) The Asmc Contributors. All rights reserved.
+; Consult your license regarding permissions and restrictions.
+;
+
+option floatdigits:31
+
+x = 3.0
+y = 1.0
+p = x
+e = 1.0e-30
+
+While ( x gt e )
+    x = x * y / ((y + 1.0) * 4.0)
+    y = y + 2.0
+    p = p + x / y
+    endm
+ifdef _WIN64
+%echo @CatStr(<64-bit: >, %p)
+else
+%echo @CatStr(<32-bit: >, %p)
+endif
+end

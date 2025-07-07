b8553561da94e66a888f6b2fbbe2707bf1231d9a
nidud <nidud@hotmail.com>
2025-07-07 15:46:55 +0200

    2.37.14

--- source/libc/string/_tcsstr.asm
+++ source/libc/string/_tcsstr.asm
@@ -3,49 +3,42 @@
 ; Copyright (c) The Asmc Contributors. All rights reserved.
 ; Consult your license regarding permissions and restrictions.
 ;
-
+; char *strstr(string1, string2) - search for string2 in string1
+;
 include string.inc
 include tchar.inc
 
+option dotname
+
     .code
 
-_tcsstr proc uses rdi rbx dst:tstring_t, src:tstring_t
+_tcsstr proc uses rbx s1:tstring_t, s2:tstring_t
 
-   .new len:size_t
+    ldr     rcx,s1
+    ldr     rdx,s2
 
-    ldr rbx,src
-
-    .if _tcslen(rbx)
-
-        mov len,rax
-        .if _tcslen(dst)
-
-            mov rcx,rax
-            xor eax,eax
-            dec len
-            mov rdi,dst
-
-            .repeat
-
-                mov _tal,[rbx]
-                repne _tscasb
-                mov _tal,0
-
-                .break .ifnz
-                .if len
-
-                    .break .if ( rcx < len )
-                     mov rdx,len
-                    .repeat
-                        mov _tal,[rbx+rdx*tchar_t]
-                        .continue(01) .if ( _tal != [rdi+rdx*tchar_t-tchar_t] )
-                        dec rdx
-                    .untilz
-                .endif
-                lea rax,[rdi-tchar_t]
-            .until 1
-        .endif
-    .endif
+    movzx   eax,tchar_t ptr [rdx]
+    test    eax,eax
+    jz      .3
+.0:
+    mov     _tal,[rcx]
+    test    _tal,_tal
+    jz      .3
+    add     rcx,tchar_t
+    cmp     _tal,[rdx]
+    jne     .0
+    xor     ebx,ebx
+.1:
+    mov     _tal,[rdx+rbx+tchar_t]
+    test    _tal,_tal
+    jz      .2
+    cmp     _tal,[rcx+rbx]
+    jne     .0
+    add     ebx,tchar_t
+    jmp     .1
+.2:
+    lea     rax,[rcx-tchar_t]
+.3:
     ret
 
 _tcsstr endp
